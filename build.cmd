1421cc5268385572987241c11048efee69ff3a20
wanggang <76527413@qq.com>
2023-01-03 09:29:21 +0800

    update

--- build.cmd
+++ build.cmd
@@ -1,4 +1,11 @@
-@ECHO OFF
+@echo off
+git status --porcelain |findstr . && goto giterror
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  echo "Working directory clean"
+else 
+  echo "Uncommitted changes"
+fi
 
 cd src/WTA.Web.UI/ &&^
 npm install &&^
@@ -8,3 +15,7 @@ dotnet restore &&^
 cd src/WTA.Web/ &&^
 dotnet publish -c Release -o ../../publish -r win-x64 --self-contained true -p:PublishSingleFile=true &&^
 cd ../../
+
+:giterror
+@echo on
+git status
