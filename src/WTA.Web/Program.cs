8d8f0032a42cba17099b7ccb0992ea8544536b9a
wanggang <76527413@qq.com>
2023-01-17 16:58:13 +0800

    update

--- src/WTA.Web/Program.cs
+++ src/WTA.Web/Program.cs
@@ -1,3 +1,28 @@
 using WTA.Infrastructure.Web.Extensions;
 
 WebApplication.CreateBuilder(args).BuildAndRun();
+
+//var aspNetCoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+//if (aspNetCoreEnvironment == "Development")
+//{
+//    builder.ConfigureAppConfiguration(delegate (HostBuilderContext _, IConfigurationBuilder cb)
+//    {
+//        try
+//        {
+//            var config = cb.Build();
+//            var address = config.GetValue<string>("NacosConfig:ServerAddresses:0")?.Trim().TrimEnd('/');
+//            AddJsonByUrl(cb, $"{address}/nacos/appsettings.json");
+//            AddJsonByUrl(cb, $"{address}/nacos/appsettings.{aspNetCoreEnvironment}.json");
+//        }
+//        catch (Exception ex)
+//        {
+//            throw new Exception($"开发模式下加载配置文件失败:{ex.Message}", ex);
+//        }
+//    });
+//}
+
+//private static void AddJsonByUrl(IConfigurationBuilder configurationBuilder, string url)
+//{
+//    var stream = new HttpClient().GetStreamAsync(url).Result;
+//    configurationBuilder.AddJsonStream(stream);
+//}
